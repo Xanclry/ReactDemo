1aae7eb4f137c00edacf0c376b119a9d98a3b027
Your Name <fed_rekish@hotmail.com>
2020-07-17 15:56:32 +0300

    Created profile page with several components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img alt={'logo'} src={'https://www.clipartmax.com/png/full/157-1576440_big-image-photography.png'}/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img src={'https://www.ejin.ru/wp-content/uploads/2019/05/gora-22.jpg'}/>
+            </div>
+            <div>
+                {/*<img src={'https://yt3.ggpht.com/a/AATXAJwUJku4i_6dn6EU--eL9dEY3fplFN2fjiPYjQ=s900-c-k-c0xffffffff-no-rj-mo'}/>*/}
+                ava + desc
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
