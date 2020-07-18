986037875e0cf5b329048d3147800f035182c961
Your Name <fed_rekish@hotmail.com>
2020-07-19 00:22:39 +0300

    Added simple dialog page

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,10 +1,29 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/1'}>Sasha</NavLink>
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to={'/dialogs/2'}>Max</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/3'}>Zahar</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/4'}>Slava</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Yo</div>
+                <div className={s.message}>How are you</div>
+            </div>
         </div>
     )
 }
