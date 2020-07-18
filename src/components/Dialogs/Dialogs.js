432e1ad76c604d63c3d0e9a2497c116f56125bc6
Your Name <fed_rekish@hotmail.com>
2020-07-19 01:40:14 +0300

    Put data aside in arrays

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -16,19 +16,34 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogItemsArray = [
+        {id : 1, name : 'Sasha'},
+        {id : 2, name : 'Max'},
+        {id : 3, name : 'Zahar'},
+        {id : 4, name : 'Slava'}
+    ]
+
+    let messagesArray = [
+        {id : 1, message : 'Hi'},
+        {id : 2, message : 'Yo'},
+        {id : 3, message : 'How are you?'},
+        {id : 4, message : 'Yo again'}
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItems}>
-                    <DialogItem id={'1'} name={'Sasha'}/>
-                    <DialogItem id={'2'} name={'Max'}/>
-                    <DialogItem id={'3'} name={'Zahar'}/>
-                    <DialogItem id={'4'} name={'Slava'}/>
+                    <DialogItem id={dialogItemsArray[0].id} name={dialogItemsArray[0].name}/>
+                    <DialogItem id={dialogItemsArray[1].id} name={dialogItemsArray[1].name}/>
+                    <DialogItem id={dialogItemsArray[2].id} name={dialogItemsArray[2].name}/>
+                    <DialogItem id={dialogItemsArray[3].id} name={dialogItemsArray[3].name}/>
             </div>
             <div className={s.messages}>
-                <Message message={'Hi'}/>
-                <Message message={'Yo'}/>
-                <Message message={'How are you'}/>
-                <Message message={'Yo again'}/>
+                <Message message={messagesArray[0].message}/>
+                <Message message={messagesArray[1].message}/>
+                <Message message={messagesArray[2].message}/>
+                <Message message={messagesArray[3].message}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,6 +3,13 @@ import React from "react";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postsArray = [
+        {id : 1, message : 'Hello! How are you?', likeCount : 12},
+        {id : 2, message : 'It\'s my first post', likeCount : 22},
+        {id : 3, message : 'Wow!', likeCount : 89},
+    ]
+
     return (
         <div className={s.wrapper}>
             <h2>My Posts</h2>
@@ -17,9 +24,9 @@ const MyPosts = () => {
                 </div>
             </div>
             <div>
-                <Post likeCount={'12'} message={'Hello! How are you?'}/>
-                <Post likeCount={'15'} message={'It\'s my first post'}/>
-                <Post likeCount={'21'} message={'Wow!'}/>
+                <Post likeCount={postsArray[0].likeCount} message={postsArray[0].message}/>
+                <Post likeCount={postsArray[1].likeCount} message={postsArray[1].message}/>
+                <Post likeCount={postsArray[2].likeCount} message={postsArray[2].message}/>
             </div>
         </div>
     )
