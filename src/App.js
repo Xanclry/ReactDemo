e31c16392600188e3de88cceebdfb3645f6e10bf
Your Name <fed_rekish@hotmail.com>
2020-07-20 01:44:21 +0300

    Created store object

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import {BrowserRouter, Route} from "react-router-dom";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 import Music from "./components/Music/Music";
-import {changeTextArea, addPost} from "./redux/state";
 
 const App = (props) => {
     return (
         <BrowserRouter>
             <div className={'app-wrapper'}>
                 <Header/>
-                <Navbar state={props.state.sidebar}/>
+                <Navbar state={props.store.getState().sidebar}/>
                 <div className={'app-wrapper-content'}>
-                    <Route path={'/dialogs'} render={() => <Dialogs state={props.state.dialogsPage} />}/>
-                    <Route path={'/profile'} render={() => <Profile state={props.state.profilePage} changeTextArea={changeTextArea} addPost={addPost}/>}/>
+                    <Route path={'/dialogs'} render={() => <Dialogs state={props.store.getState().dialogsPage} />}/>
+                    <Route path={'/profile'} render={() => <Profile state={props.store.getState().profilePage}
+                                                                    changeTextArea={props.store.changeTextArea.bind(props.store)}
+                                                                    addPost={props.store.addPost.bind(props.store)}/>}/>
                     <Route path={'/news'} render={() => <News/>}/>
                     <Route path={'/music'} render={() => <Music/>}/>
                     <Route path={'/settings'} render={() => <Settings/>}/>
---
+++ src/redux/store.js
@@ -0,0 +1,65 @@
+let store = {
+    _state: {
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Sasha'},
+                {id: 2, name: 'Max'},
+                {id: 3, name: 'Zahar'},
+                {id: 4, name: 'Slava'},
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'Yo'},
+                {id: 3, message: 'How are you?'},
+                {id: 4, message: 'Yo again'},
+            ]
+        },
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hello! How are you?', likeCount: 12},
+                {id: 2, message: 'It\'s my first post', likeCount: 22},
+                {id: 3, message: 'Wow!', likeCount: 89},
+            ],
+            textareaValue: '123'
+        },
+        sidebar: {
+            friends: [
+                {
+                    name: 'Andrew',
+                    picture: 'https://i.pinimg.com/736x/a6/2f/d9/a62fd99e65131cacfbfc5a6b1816a4ec.jpg'
+                },
+                {
+                    name: 'Elena',
+                    picture: 'https://i.pinimg.com/736x/2f/64/7e/2f647e6a038742adaea5a3cbcd36ba68--portrait-wall-woman-portrait.jpg'
+                },
+                {
+                    name: 'Roman',
+                    picture: 'https://i05.fotocdn.net/s119/1da13d0280515168/user_l/2711201444.jpg'
+                },
+            ]
+        }
+    },
+    getState() {
+        return this._state
+    },
+    _callSubscriber() {
+        console.log('State was changed')
+    },
+
+    addPost() {
+        debugger
+        this._state.profilePage.posts.push({id: 4, message: this._state.profilePage.textareaValue, likeCount: 0})
+        this._state.profilePage.textareaValue = ''
+        this._callSubscriber(store)
+    },
+    changeTextArea(newValue) {
+        this._state.profilePage.textareaValue = newValue
+        this._callSubscriber(store)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer
+    }
+}
+
+
+export default store
